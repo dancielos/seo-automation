9595cc83c73805af005036e0e1687baa09e0aa9e
Dan <iam@dancielos.com>
2024-06-17 11:01:28 -0600

    made a simple ngram filter

--- filter-keywords.js
+++ filter-keywords.js
@@ -1,3 +1,22 @@
-function filterKeywords() {
-	return 'hello world!';
+function getNGrams(values, n = 1) {
+	const nGrams = new Map();
+	values.forEach((val) => {
+		const words = ('' + val).split(' ');
+
+		words.forEach((word) => {
+			const frequency = nGrams.get('' + word) ?? 0;
+			// Logger.log({word, frequency})
+			nGrams.set('' + word, frequency + 1);
+		});
+	});
+	return Array.from(nGrams.entries());
+}
+
+function testGetNGrams() {
+	Logger.log('testing...');
+	const sheet = SpreadsheetApp.getActiveSheet();
+	const values = sheet.getRange('A2:A20').getValues();
+
+	Logger.log(values);
+	Logger.log(getNGrams(values));
 }
