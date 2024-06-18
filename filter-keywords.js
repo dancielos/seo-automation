8617bc8c31e9eafedea91163071d28e6d301a305
Dan <iam@dancielos.com>
2024-06-18 08:30:59 -0600

    implemented word exclusion

--- filter-keywords.js
+++ filter-keywords.js
@@ -1,12 +1,44 @@
+const COMMON_WORDS = [
+	'a',
+	'is',
+	'the',
+	'are',
+	'they',
+	'their',
+	'theirs',
+	'them',
+	'of',
+	'i',
+	'on',
+	'this',
+	'be',
+	'in',
+	'an',
+	'that',
+];
+const EXCLUDED_WORDS_MAP = new Map();
+
+COMMON_WORDS.forEach((w) => EXCLUDED_WORDS_MAP.set(w, 1));
+
+// TODO
+// [] group similar words like...
+// US, u.s.
+// review, reviews
+// builder, builders
+// [] implement multigram, which should make use of the sliding window algo
+// using that algo will be faster as it will run at O(n) instead of O(ngram * n)
+// [] implement the volume per keyword and count those too
+
 function getNGrams(values, n = 1) {
+	// to implement
 	const nGrams = new Map();
 	values.forEach((val) => {
-		const words = ('' + val).split(' ');
+		const words = ('' + val).toLowerCase().split(' ');
 
 		words.forEach((word) => {
 			const frequency = nGrams.get('' + word) ?? 0;
 			// Logger.log({word, frequency})
-			nGrams.set('' + word, frequency + 1);
+			if (!EXCLUDED_WORDS_MAP.get(word)) nGrams.set('' + word, frequency + 1);
 		});
 	});
 	return Array.from(nGrams.entries());
